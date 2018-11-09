3b9cbf39ff5da235446782f15677f841a0dcc61f
Jack Ling <j.lingyx@gmail.com>
2018-11-09 12:25:27 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,5 @@
 def prime?(int)
+<<<<<<< HEAD
  
   if int <= 1 
     return false
@@ -11,4 +12,9 @@ def prime?(int)
     end
   end
   return true
+=======
+  if int < 3
+    return true
+  end
+>>>>>>> 2e63bb773eb45c99aaf24b5c8f6c7ee0a2c77cc1
 end

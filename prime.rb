39d08222a5c6aeb99c220e7d2e6326355e6c6ff0
Jack Ling <j.lingyx@gmail.com>
2018-11-09 12:32:22 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,14 @@
-def prime?(number)
-  (2..number). each do |divisor|
-    if number % divisor == 0 
-      return TRUE
-    else
-      return FALSE
+def prime?(int)
+ 
+  if int <= 1 
+    return false
+  elsif int > 0 && int < 3
+    return true
+  end
+  for i in 2..int-1
+    if (int % i) == 0
+      return false
     end
   end
+  return true
 end
